3d16d20c2ae2c6d99f9818aef206fc266f4f53e4
Franky <frankyan2001@gmail.com>
2025-06-03 01:29:48 -0400

    Modificando el app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Information from './components/Information';
+import Services from './components/Services';
+import Events from './components/Events';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Information />
+      <Services />
+      <Events />
+      <Testimonials />
+      <Contact />
     </div>
   );
 }
