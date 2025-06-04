1092c8388c47d10e75badc8c13df2ca5489c46b5
Franky <frankyan2001@gmail.com>
2025-06-04 01:45:55 -0400

    Creando Mobile menu button en el componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,8 +31,22 @@ const Header = () => {
             </div>
           </div>
 
-          
-      )}
+          {/* Mobile menu button */}
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            >
+              {isMenuOpen ? (
+                <XMarkIcon className="block h-6 w-6" />
+              ) : (
+                <Bars3Icon className="block h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
     </nav>
   );
 };
