9f5d3ac076c4de2470a042cf5f24c591b8596b4e
Franky <frankyan2001@gmail.com>
2025-06-05 01:59:42 -0400

    Iniciando componente Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section id="inicio" className="min-h-screen bg-gradient-to-br from-amber-50 to-green-50 relative overflow-hidden">
+      
+    </section>
+  );
+};
+
+export default Hero;
