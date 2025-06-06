417bdd333a05f5fa698cfc842ed51adeccf0b9ea
Franky <frankyan2001@gmail.com>
2025-06-06 10:13:41 -0400

    Creando las Info cards en el componente Information

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 const Information = () => {
+  const infoCards = [
+    {
+      title: "Transformamos cada espacio en un refugio zen",
+      description: "Cada bonsai es único, cada espacio es especial. Creamos armonía perfecta.",
+      image: "https://images.unsplash.com/photo-1585544224318-147fb1bcc462"
+    },
+    {
+      title: "Adaptamos el modelo de ecosistema natural",
+      description: "Respetamos la naturaleza y sus ciclos, creando equilibrio duradero.",
+      image: "https://images.unsplash.com/photo-1713372086778-cb578f06b866"
+    },
+    {
+      title: "Innovamos en poner la tradición milenaria",
+      description: "Combinamos técnicas ancestrales con innovación moderna.",
+      image: "https://images.pexels.com/photos/732423/pexels-photo-732423.jpeg"
+    }
+  ];
+
+  
 };
 
 export default Information;
