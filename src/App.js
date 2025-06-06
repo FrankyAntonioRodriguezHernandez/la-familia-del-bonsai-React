ece59f1bb8ee10e29524475b9e9743d16e46af38
Franky <frankyan2001@gmail.com>
2025-06-06 10:13:41 -0400

    Creando el componente Informatio

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Information from './components/Information';
+/*import Information from './components/Information';
 import Services from './components/Services';
 import Events from './components/Events';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Hero />
+Abajo poner 
       <Information />
       <Services />
       <Events />
       <Testimonials />
       <Contact />
+*/
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      
     </div>
   );
 }
