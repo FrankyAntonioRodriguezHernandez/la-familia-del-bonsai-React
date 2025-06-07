d418e6314c1d14d71d4cb0d8c9a8d086ab83e886
Franky <frankyan2001@gmail.com>
2025-06-07 10:38:26 -0400

    Creando el componente Services

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
-/*import Information from './components/Information';
+import Information from './components/Information';
+/*
 import Services from './components/Services';
 import Events from './components/Events';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 
 Abajo poner 
-      <Information />
+      
       <Services />
       <Events />
       <Testimonials />
@@ -20,6 +21,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      <Information />
       
     </div>
   );
