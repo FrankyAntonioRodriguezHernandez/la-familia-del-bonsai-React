887e016188bac0254109b3a0b04940cb1ddd8f6f
Franky <frankyan2001@gmail.com>
2025-06-07 10:38:26 -0400

    Incorporando el componente Services

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Information from './components/Information';
-/*
 import Services from './components/Services';
+/*
 import Events from './components/Events';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 
 Abajo poner 
       
-      <Services />
+      
       <Events />
       <Testimonials />
       <Contact />
@@ -22,7 +22,8 @@ function App() {
       <Header />
       <Hero />
       <Information />
-      
+      <Services />
+
     </div>
   );
 }
