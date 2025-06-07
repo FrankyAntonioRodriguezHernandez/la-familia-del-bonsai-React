dce1d229a312c701ce22be93dbffe7b850581276
Franky <frankyan2001@gmail.com>
2025-06-07 10:38:26 -0400

    Creando los servicios en el componente Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 const Services = () => {
+  const services = [
+    {
+      title: "Poda y técnica de bonsai",
+      description: "Más que una creación, una experiencia que se alimenta de naturaleza",
+      image: "https://images.pexels.com/photos/7127356/pexels-photo-7127356.jpeg",
+      buttonText: "Ver"
+    },
+    {
+      title: "Arrendamiento de bonsai",
+      description: "Le ofrecemos la oportunidad de tener un bonsai como experiencia para eventos especiales",
+      image: "https://images.pexels.com/photos/1198741/pexels-photo-1198741.jpeg",
+      buttonText: "Ver"
+    },
+    {
+      title: "Decoración de eventos",
+      description: "Transformamos su evento en una experiencia única, integrando elegancia y armonía",
+      image: "https://images.pexels.com/photos/27113617/pexels-photo-27113617.jpeg",
+      buttonText: "Ver"
+    }
+  ];
+
 };
 
 export default Services;
