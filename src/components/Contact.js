b02139b9587ad1f2ba33663e710f5a46d8bd414e
Franky <frankyan2001@gmail.com>
2025-06-11 09:47:18 -0400

    Iniciando el componente Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/outline';
+import { FaWhatsapp, FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
+
+const Contact = () => {
+};
+
+export default Contact;
