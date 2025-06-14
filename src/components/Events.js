0ddecfcda04a42170168fc3548048d78bf762459
Franky <frankyan2001@gmail.com>
2025-06-14 12:00:00 -0400

    Cambiando fotos en Events

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import Taller1 from '../assets/Taller1.jpg'
+import Exposicion1 from '../assets/Exposicion1.jpg'
+import MejorMomento1 from '../assets/MejorMomento1.jpg'
 
 const Events = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const events = [
     {
-      title: "Mejores Momentos",
-      description: "Guiamos a través de las estaciones, revelando el momento ideal para cada paso en el arte del bonsai, asegurando su crecimiento armonioso y saludable.",
-      image: "https://images.unsplash.com/photo-1657043496762-89aca96457a0",
+      title: "Exposiciones de Arte",
+      description: "Celebramos la belleza del bonsai en exposiciones que muestran la armonía entre naturaleza y técnica artística tradicional.",
+      image: Exposicion1,
       buttonText: "Ver"
     },
     {
       title: "Talleres Especializados",
       description: "Eventos educativos donde compartimos técnicas milenarias y modernas para el cuidado perfecto de sus bonsai en cada estación del año.",
-      image: "https://images.unsplash.com/photo-1614797091849-05915639d85a",
+      image: Taller1,
       buttonText: "Ver"
     },
     {
-      title: "Exposiciones de Arte",
-      description: "Celebramos la belleza del bonsai en exposiciones que muestran la armonía entre naturaleza y técnica artística tradicional.",
-      image: "https://images.unsplash.com/photo-1585544224318-147fb1bcc462",
+      title: "Mejores Momentos",
+      description: "Guiamos a través de las estaciones, revelando el momento ideal para cada paso en el arte del bonsai, asegurando su crecimiento armonioso y saludable.",
+      image: MejorMomento1,
       buttonText: "Ver"
-    }
+    },
   ];
 
   const nextSlide = () => {
