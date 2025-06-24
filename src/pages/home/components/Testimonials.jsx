1ca38d9e7468dd050a67c397bdc9ee63039a56a6
Franky <frankyan2001@gmail.com>
2025-06-24 12:00:00 -0400

    redimensionando las tarjetas de los testimonios

--- src/pages/home/components/Testimonials.jsx
+++ src/pages/home/components/Testimonials.jsx
@@ -33,7 +33,7 @@ const Testimonials = () => {
     {
       name: "Isabel Torres",
       text: "Profesionales excepcionales, sus bonsais son verdaderas obras de arte",
-      rating: 4.5,
+      rating: 5,
       avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=150&h=150&fit=crop&crop=face"
     }
   ];
@@ -54,18 +54,27 @@ const Testimonials = () => {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 1,
+  slidesToShow: 3,
   slidesToScroll: 1,
   centerMode: true,
-  centerPadding: "60px", 
+  centerPadding: "80px", 
   autoplay: true,
   autoplaySpeed: 5000,
   responsive: [
     {
+      breakpoint: 1024, 
+      settings: {
+        slidesToShow: 3,
+        centerMode: true,
+        centerPadding: "40px",
+      },
+    },
+    {
       breakpoint: 768, 
       settings: {
-        centerMode: false,
-        centerPadding: "0px",
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "30px",
       },
     },
   ],
