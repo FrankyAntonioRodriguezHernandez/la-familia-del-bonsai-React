f638f8bd66889a35c5b19da9ef2cd474251693e1
Franky <frankyan2001@gmail.com>
2025-06-25 12:00:00 -0400

    Agregando Switch de modo oscuro en components

--- src/pages/home/components/Hero.jsx
+++ src/pages/home/components/Hero.jsx
@@ -1,6 +1,5 @@
 
 import BonsaiTreeBonania1 from '../../../images/Hero/BonsaiTreeBonania1.jpg';
-import DarkLighModeSwitch from '../../../components/DarkLighModeSwitch';
 
 const Hero = () => {
   return (
@@ -28,7 +27,6 @@ const Hero = () => {
                 TRANSFORMAMOS SUS ESPACIOS EN<br />
                 ENTORNOS ARMONIOSOS
               </p>
-              <DarkLighModeSwitch/>
               <button className="bg-emerald-600 hover:bg-emerald-700 text-white px-8 py-3 rounded-full text-lg font-semibold transition-colors duration-300 shadow-lg">
                 Ver Catálogo
               </button>
